0de92790a6d1932b9876fa2843f3b54f98665fbe
Dhairya Surana <dhairs7@vt.edu>
2018-11-26 05:59:13 -0500

    Created functions for the intro message and the main game itself

--- ZombieGame.py
+++ ZombieGame.py
@@ -2,6 +2,35 @@
 
 #Creators: Dhairya Surana and Luke Knoble
 
-
 #==================================================================================================================================================
 
+import curses
+import threading
+
+def introMessage():
+    
+    print("The Zombie game will be launched in 5 seconds")
+    
+def runGame():
+
+    screen = curses.initscr()
+    curses.curs_set(0)
+    sh, sw = screen.getmaxyx()
+    w = curses.newwin(sh, sw, 0, 0)
+    w.keypad(1)
+    w.timeout(100)
+
+    key = w.getch()
+
+    while (True):
+        
+        if(key == curses.KEY_ENTER):
+            curses.endwin()
+            quit()
+       
+
+     
+      
+introMessage()
+timer = threading.Timer(5.0, runGame)
+timer.start()
