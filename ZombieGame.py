cdaedc75e4325dbb0f1d64735d45c2464de0bfe2
Dhairya Surana <dhairs7@vt.edu>
2018-12-06 16:19:28 -0500

    Fixed the function that prints text slowly and changed the intro message

--- ZombieGame.py
+++ ZombieGame.py
@@ -6,32 +6,20 @@
 
 import curses
 import time
-from math import *
+import sys
 
-
-def introMessage():
-
-    for second in range(5, 0, -1):
-        
-       if(second != 1):
-           print("The Zombie game will be launched in " + str(second) + " seconds", end = "\r")
-       else:
-           print("The Zombie game will be launched in " + str(second) + " second ", end = "\r")
-
-       time.sleep(1)
-
-    
 
 def printTextSlowly(text):
 
     for character in text:
-        print(character, end = '')
+        print(character, end = "")
         time.sleep(0.125)
+        sys.stdout.flush()
 
-
-def displayIntroScreen():
+def introMessage():
 
     printTextSlowly("Sudochad Stud|os presents...")
+    time.sleep(3)
 
 def runGame():
 
@@ -42,19 +30,13 @@ def runGame():
     w = curses.newwin(sh, sw, 0, 0)
     w.keypad(1)
     w.border(0)
+   # w.addstr(x = 5, y = 5, "Gay")
     w.timeout(100)
-    curses.initscr()
-    
     
     while (True):
 
         key = w.getch()
-        
 
-
-#printTextSlowly("gay man")
-#introMessage()
-displayIntroScreen()
-#runGame()
-
+introMessage()
+runGame()
 
