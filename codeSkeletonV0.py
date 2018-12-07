dab972116de72f1f1fe2d4ba3060d4f45490476a
Desync-o-tron <deltav@vt.edu>
2018-12-06 23:59:41 -0500

    Ayyyy python rocks! Code layout has been arrang

--- codeSkeletonV0.py
+++ codeSkeletonV0.py
@@ -1,35 +1,56 @@
 import random
 import curses
+import time
+import sys
 
+# TODO come to a conclusion on how we'll be sharing all the variables..
+# to-be shared variables:
+# window
+# map .. aka objects on and off the screen aka a big ass array.. BAH NO - lets use a struct!
+# key pressed.. actually not nessecary (we can run get key in each function)
+
+#def init_screen(window): 
 s = curses.initscr()
 curses.curs_set(0)
 sh, sw = s.getmaxyx() #TODO CREATE A SET SCREEN SIZE.
 window = curses.newwin(sh, sw, 0, 0)
-window.nodelay(True)
+window.nodelay(True)    #does this actually work?
 window.keypad(1)    #What does this do?
-window.timeout(100)
+window.timeout(100) #and this?
 
-hero_x = sw/4
-hero_y = sh/2
-hero = [
-    [hero_y, hero_x],
-    [hero_y, hero_x-1],
-]
+class ze_map_class:
+    hero = [ [0,0],[0,0] ] # change size as needed.
+    
 
-#window.addch(int(food[0]), int(food[1]), int(curses.ACS_PI))
+def init_map(ze_map):
+    hero_x =  sw / 4
+    hero_y =  sh / 2
 
-key = curses.KEY_RIGHT
+    ze_map.hero = [
+        [hero_y, hero_x],
+        [hero_y, hero_x-1],
+    ] 
 
-while True:
-    next_key = window.getch()
-    #key = key if next_key == -1 else next_key
-    key = next_key
 
-    #delete old character pos..optimize later - lets see how python can handle it
-    window.addch(int(hero[0][0]),int(hero[0][1]), ' ')
-    window.addch(int(hero[1][0]),int(hero[1][1]), ' ')
+def display_title():
+    sys.stdout.flush()
+    text = "Sudochad Stud|os presents..."
+    for character in text:
+        print(character, end = "")
+        #time.sleep(0.125) #sexy but too slow for debugging
+        time.sleep(.03)
+        sys.stdout.flush()
+    time.sleep(.12)
 
-    new_hero_head = [hero[0][0], hero[0][1]]
+
+def move_hero(ze_map):
+    key = window.getch()   #TODO figure out how to grab 2 keys at once for moving and shooting, etc..
+
+    #delete old character pos..optimize later - lets see how python can handle it - also it seems like the type of terminal is relevant to updating speed.. research DOM terminal/shell?
+    window.addch(int(ze_map.hero[0][0]),int(ze_map.hero[0][1]), ' ')
+    window.addch(int(ze_map.hero[1][0]),int(ze_map.hero[1][1]), ' ')
+
+    new_hero_head = [ze_map.hero[0][0], ze_map.hero[0][1]]
 
     if key == curses.KEY_DOWN:
         new_hero_head[0] += 1
@@ -40,11 +61,30 @@ while True:
     if key == curses.KEY_RIGHT:
         new_hero_head[1] += 1
 
-    #update hero pos array
-    hero[0] = new_hero_head
-    hero[1] = [hero[0][0] + 1, hero[0][1]]
+    #update ze_map.hero pos array
+    ze_map.hero[0] = new_hero_head
+    ze_map.hero[1] = [ze_map.hero[0][0] + 1, ze_map.hero[0][1]]
 
-    window.addch(int(hero[0][0]),int(hero[0][1]), '8')
-    window.addch(int(hero[1][0]),int(hero[1][1]), '0')
+    window.addch(int(ze_map.hero[0][0]),int(ze_map.hero[0][1]), '8')
+    window.addch(int(ze_map.hero[1][0]),int(ze_map.hero[1][1]), '0')
 
+
+def main():
+    ze_map = ze_map_class()
+    init_map(ze_map)
+
+    display_title()
+
+    while True: #TODO stop shit from going off screen and breaking the program lol
+        move_hero(ze_map)
+        #move_baddies(ze_map)
+
+
+"""
+
+ ◖========8 ############################## leggo baby. ################################ 8========D
+
+"""
+
+main()
 
