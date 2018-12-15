f207b0960e9f0135efe68bd53e3af8121e0a64d5
Dhairya Surana <dhairyasurana@localhost.localdomain>
2018-12-14 20:28:59 -0500

    Added sudo to the installer

--- installer.py
+++ installer.py
@@ -7,16 +7,19 @@ import sys
 
 
 def install(package):
-     subprocess.call([sys.executable, "-m", "pip", "install", package])
+     subprocess.call(["sudo", sys.executable, "-m", "pip", "install", package])
 
-package_names = ["numpy", "pygame", "curses", "pip"]
+def main():
+    package_names = ["numpy", "pygame", "curses"]
 
-for package_name in package_names:
+    for package_name in package_names:
 
-    spec = importlib.util.find_spec(package_name)
-    if spec is None:
-        print(package_name +" is not installed")
-        install(package_name)
-        print(package_name + "has now been installed")
-    else:
-        print(package_name + " is installed")
+        spec = importlib.util.find_spec(package_name)
+        if spec is None:
+            print(package_name +" is not installed")
+            install(package_name)
+            print(package_name + "has now been installed")
+        else:
+            print(package_name + " is installed")
+
+main()
